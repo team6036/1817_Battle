5c84217cfcc639ac871ad052bf229996eaf15b97
frc6036 <frc6036@gmail.com>
2021-09-30 21:41:07 -0700

    fixed swerve constants - check back 2 commits for previous code branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -144,28 +144,28 @@ public class Constants {
             public static final int D = 4;
             public static final int T = 3;
             public static final int E = 10;
-            public static final double offset = Util.toEncoderTicks(275);
+            public static final double offset = 2.727418 + Math.PI;
         }
 
         public static final class FL {
             public static final int D = 6;
             public static final int T = 5;
             public static final int E = 11;
-            public static final double offset = Util.toEncoderTicks(215);
+            public static final double offset = 2.508059 + Math.PI;
         }
 
         public static final class BR {
             public static final int D = 2;
             public static final int T = 1;
             public static final int E = 9;
-            public static final double offset = Util.toEncoderTicks(195);
+            public static final double offset = 0.632000 - 0.47123889 + 2.819764;
         }
 
         public static final class BL {
             public static final int D = 8;
             public static final int T = 7;
             public final static int E = 12;
-            public final static double offset = Util.toEncoderTicks(190);
+            public final static double offset = 0.878971 + Math.PI;
         }
     }
 
